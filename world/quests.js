4589d51472bb58533dabe2b13802d2fff3a2e746
Kinora <wanbizon61@gmail.com>
2026-06-12 21:57:44 +0700

    Create quests.js

---
+++ world/quests.js
@@ -0,0 +1,20 @@
+export const quests = [
+  {
+    id: 1,
+    title: "Lost Relic",
+    reward: 100,
+    currency: "KINORA"
+  },
+  {
+    id: 2,
+    title: "Ancient Guardian",
+    reward: 250,
+    currency: "KINORA"
+  },
+  {
+    id: 3,
+    title: "Mystic Expedition",
+    reward: 500,
+    currency: "KINORA"
+  }
+];
